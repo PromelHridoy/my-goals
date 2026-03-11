8ab5ae12844fe38eecfe44879d58afd69f9ca212
PromelHridoy <promel.hridoy12@gmail.com>
2026-03-11 20:52:33 +0600

    I explored string methods

---
+++ string-methods/length.js
@@ -0,0 +1,78 @@
+// let str1 = "JavaScript";
+// let str2 = "I love JS";
+// console.log(str1.length);
+// console.log(str2.length);
+
+// let str = "JavaScript";
+// console.log(str.charAt(0));
+// console.log(str.charAt(str.length - 1));
+// const lestStr = str.charAt(str.length - 1);
+// console.log(lestStr);
+
+// const str11 = "JavaScript";
+// console.log(str11.charCodeAt());
+// console.log(str11.slice(0,4));
+// console.log(str11.substring(4));
+
+// let str12 = "JavaScript";
+// let str22 = "Hello World";
+// console.log(str12.indexOf('v'));
+// console.log(str22.indexOf('r'));
+// console.log(str12.lastIndexOf('ip'));
+// console.log(str12.toLowerCase().startsWith("java"));
+// console.log(str12.toLowerCase().endsWith("pt"));
+
+// let str21 = "Hello World";
+// console.log(str21.toLowerCase().replace("world", "Js"));
+// let strB = "banana";
+// console.log(strB.replaceAll("a", "A"));
+// const trim = " banana ";
+// console.log(trim);
+// console.log(trim.trim().repeat(7));
+// console.log(trim.trimStart().repeat(2));
+// console.log(trim.trimEnd().repeat(3));
+// console.log(trim.trimStart().repeat(4));
+
+// let str88 = "I love JavaScript";
+// console.log(str88.split(" "));
+// let fruits = "apple,banana,orange";
+// console.log(fruits.split(","));
+
+// let num = 123;
+// let bool = true;
+// const tostring = num.toString();
+// console.log(num.toString());
+// console.log(bool.toString());
+// console.log(typeof(tostring));
+
+// let name = "Alice";
+// let age = 25;
+// console.log(`My name is ${name} and I am ${age} years old`);
+
+// let strMul;
+// strMul = `I love Js
+// I love coding
+// `
+// console.log(strMul);
+
+// let a = 5;
+// let b = 10;
+// console.log(`${a} + ${b} = ${a + b}`);
+// const sum = a + b;
+// console.log(sum);
+
+
+let str = "abc123xyz456";
+console.log(str.match(/\d/g));
+
+
+
+
+
+
+
+
+
+
+
+
